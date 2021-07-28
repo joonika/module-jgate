466afa1f3e0ceae3e877ca8f8040c8dccbba683c
mnjadidi <mnjadidi@gmail.com>
2021-07-28 19:31:54 +0430

    fix

--- src/Views/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-$test = \Modules\jgate\src\jgate::request('test',[]);
-jdie($test);
-//$data = Joonika\Modules\Gateway\Gateway::callApi('nationalDriver',['code'=>"4011755771"]);
-echo hr_html();
-dd($data);
